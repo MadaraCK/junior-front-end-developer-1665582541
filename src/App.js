ee3b8ac16c393661548ee8ac811375db366c77f7
Mikołaj <radek_mikolaj@o2.pl>
2022-10-13 10:17:20 +0200

    desktop done

--- src/App.js
+++ src/App.js
@@ -160,6 +160,40 @@ function App() {
                 <h1 className="title_app">
                   Application has been accepted 🎉 🙌
                 </h1>
+                <section className="img_message">
+                  <div className="profil_img">
+                    <div className="profil" />
+                  </div>
+                  <div className="message_name">
+                    <div className="name_date">
+                      <p className="nick_name">Kirsten Aniston</p>
+                      <p className="date_nick">
+                        • Today, 17th December • 11:20
+                      </p>
+                    </div>
+                    <p className="text_kirsten kirsten_first">Hello!</p>
+                    <p className="text_kirsten">
+                      My name is Kirsten, and I'm super happy to announce that
+                      your application to join Coders Family has been accepted!
+                      🎉 🙌
+                    </p>
+                    <p className="text_kirsten">
+                      You really impressed us during the interview process, and
+                      we'd like to offer you a project with nerds.family. We've
+                      been working with nerds.family for quite some time, and
+                      it's important for us to keep them satisfied with our
+                      services — hence why we believe you'll be a great fit for
+                      this job.
+                    </p>
+                    <p className="text_kirsten">
+                      Here's the project overview: You'll be responsible for
+                      building a database and page for recording and displaying
+                      book ratings. We'd like to work in two week-long sprints,
+                      we've already given you a head start and prepared your
+                      tasks to deliver in your first sprint.
+                    </p>
+                  </div>
+                </section>
               </section>
             </div>
           </section>
